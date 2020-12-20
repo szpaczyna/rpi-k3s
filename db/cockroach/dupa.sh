314003fbc69cdcf724bf13d6bda9f20a9064d0b0
szpaczyna <shpaq@shpaq.org>
2020-12-20 12:16:29 +0100

    cockroachdb

---
+++ db/cockroach/dupa.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker buildx create --use
+docker buildx inspect --bootstrap
+docker buildx build --load --platform "linux/arm64" -t cockroachdb:v20.2.3 .
