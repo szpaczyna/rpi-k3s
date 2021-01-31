686b401c54a8e430cb39c4f327e6481cae636ade
shpaq <shpaq@shpaq.org>
2021-01-31 14:29:17 +0100

    proxy update

--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -1,11 +1,11 @@
 ## global
-export http_proxy="http://10.0.0.13:3128/"                                                                                                 
-export https_proxy="http://10.0.0.13:3128/"                                                                                                
-export ftp_proxy="http://10.0.0.13:3128/"                                                                                                  
-export no_proxy="127.0.0.1,localhost"                                                                                                      
+export http_proxy="http://10.0.0.2:3128/"
+export https_proxy="http://10.0.0.2:3128/"
+export ftp_proxy="http://10.0.0.2:3128/"
+export no_proxy="127.0.0.1,localhost"
                                                                                                                                            
-## curl                                                                                                                                 
-export HTTP_PROXY="http://10.0.0.13:3128/"                                                                                                 
-export HTTPS_PROXY="http://10.0.0.13:3128/"                                                                                                
-export FTP_PROXY="http://10.0.0.13:3128/"                                                                                                  
-export NO_PROXY="127.0.0.1,localhost"  
+## curl
+export HTTP_PROXY="http://10.0.0.2:3128/"
+export HTTPS_PROXY="http://10.0.0.2:3128/"
+export FTP_PROXY="http://10.0.0.2:3128/"
+export NO_PROXY="127.0.0.1,localhost"
