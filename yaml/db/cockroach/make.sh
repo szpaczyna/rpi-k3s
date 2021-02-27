d09b4ebff34a357ecdf151a7b640218142491f76
shpaq <shpaq@shpaq.org>
2021-02-28 00:15:14 +0100

    remove gogs

--- yaml/db/cockroach/make.sh
+++ yaml/db/cockroach/make.sh
@@ -3,4 +3,4 @@ apt -y install qemu-user-static
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx create --use
 docker buildx inspect --bootstrap
-docker buildx build --load --platform "linux/arm64" -t cockroachdb:v20.2.3 .
+docker buildx build --load --platform "linux/arm64" -t cockroachdb:v20.2.5 .
