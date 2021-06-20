c4ec46be9da2bc61a380d869766c34b726d89aad
shpaq <shpaq@shpaq.org>
2021-06-20 13:26:55 +0200

    fix: labels updated to new standard

--- scripts/rpi_temp.sh
+++ scripts/rpi_temp.sh
@@ -5,5 +5,6 @@ export LD_LIBRARY_PATH=/opt/vc/lib
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
-echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp | sed -e 's/temp=//g')"
+#echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp | sed -e 's/temp=//g')"
+echo "USB Disk => $(smartctl -A /dev/sda | awk '/Temperature_Celsius/ {print $10}')'C"
 echo "CPU => $((cpu/1000))'C"
