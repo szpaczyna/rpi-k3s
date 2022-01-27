ac78b99a1241a52b3a45cac029a064e76471f481
shpaq <shpaq@shpaq.org>
2022-01-27 10:04:23 +0100

    feat: cert-manager version bump

--- cluster/core/cert-manager/deploy-cert-manager.sh
+++ cluster/core/cert-manager/deploy-cert-manager.sh
@@ -8,7 +8,7 @@ helm upgrade --install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.6.1 \
+  --version v1.7.0 \
   --set installCRDs=true \
   --set prometheus.enabled=true
 
