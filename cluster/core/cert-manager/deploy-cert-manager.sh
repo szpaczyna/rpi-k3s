c44b536fd761fd4785076e0e3b2e3926a6f91b3e
shpaq <shpaq@shpaq.org>
2022-10-21 14:23:47 +0200

    feat: longhorn/bitwarden/nextcloud version bump, apple health dashboard for influxdb

--- cluster/core/cert-manager/deploy-cert-manager.sh
+++ cluster/core/cert-manager/deploy-cert-manager.sh
@@ -8,7 +8,7 @@ helm upgrade --install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.9.1 \
+  --version v1.10.0 \
   --set installCRDs=true \
   --set prometheus.enabled=true
 
