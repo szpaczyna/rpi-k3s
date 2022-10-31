15968e1726ce844d93cc091e791cfad525811a57
shpaq <shpaq@shpaq.org>
2022-10-31 13:21:35 +0100

    feat: updated script for checking temperature

--- scripts/rpi_temp.sh
+++ scripts/rpi_temp.sh
@@ -5,6 +5,7 @@ export LD_LIBRARY_PATH=/opt/vc/lib
 cpu=$(</sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)"
 echo "-------------------------------------------"
-#echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp | sed -e 's/temp=//g')"
-echo "USB Disk => $(smartctl -A /dev/sda | awk '/Temperature_Celsius/ {print $10}')'C"
+echo "GPU => $(/opt/vc/bin/vcgencmd measure_temp | sed -e 's/temp=//g')"
+echo "SSD Disk => $(smartctl -A /dev/sda | awk '/Temperature_Celsius/ {print $10}')'C"
+echo "USB Disk => $(smartctl -A /dev/sdb | awk '/Temperature_Celsius/ {print $10}')'C"
 echo "CPU => $((cpu/1000))'C"
