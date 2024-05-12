730b2beb227d5ce1a6a2d2238eb2668236cfbda7
shpaq <shpaq@shpaq.org>
2024-05-12 19:24:26 +0200

    feat: he.net tunnel script

---
+++ cluster/secrets/erx-he-tunnel.sh
@@ -0,0 +1,21 @@
+{
+	"data": "ENC[AES256_GCM,data:nSmdHe86eQi/X9RJspOqFgJ8qFTyPr/3QoRd9ZGnA2+3TLJClgTaIlxtgcDruJ3nzMMKT5eB0d4xj5sGHVDudZlxjpaY0x2QBS4YOMgK/NLALmGBm+pdvhdRllQDfdgp5Ow2tej+30VJYplkQKgaDVVr6ed0NNT9zsmbaoIP5hV8UdPd7AF7ivRboUxuq+F2OaopuxDlMwD+hFcrTZ/e0U4OZustuLOHhwW9GPGxkFyjl+V4zpyJ89PvtoHMsIH+nquVyOZTUNnK6Azi14UyZ20blzmH7XDr68blPMwuzBHBm8e46aCFjmEqAhaTwx56K3n9aGBlw3hTxH+UrwFX3marYa2jy1xo,iv:y9YB++hnh4OcP7pr7GwlPvmGNFOqenxx3XkEnM8IJM4=,tag:N/wt96NPZGoVQPh8RroiXg==,type:str]",
+	"sops": {
+		"kms": null,
+		"gcp_kms": null,
+		"azure_kv": null,
+		"hc_vault": null,
+		"age": null,
+		"lastmodified": "2024-05-12T17:23:21Z",
+		"mac": "ENC[AES256_GCM,data:omhdQhfhz8Vz6/ppnpjtGwS02sHCE4ioyZAk0qc5SqgL0C4AU0Wxn7R+t6qSyU1sArb2qhuoKBkOFteZhh8qPaUAvpzysrlciQVIRM8GlkoIuqscyqD5+UdumMLV2dm5leuwJOZFxtYpbUt+3OepIC4FVjKnB8kWK4Aj1sMiiBs=,iv:Y+tOzcQ1La/cKDW77/PFQmIC+s1/mhPidaQJHMwkIUs=,tag:wNMARNwkxMpRxSb84xF1mA==,type:str]",
+		"pgp": [
+			{
+				"created_at": "2024-05-12T17:05:23Z",
+				"enc": "-----BEGIN PGP MESSAGE-----\n\nhQIMA4APDMemWJ+LARAAs1svJhY0LxkdAMhNvcIIho2YsjJwNu/l5ahmMZlwFVni\nxBZMFTfRlk+zdPx/8oZA/27bkVvNWGzl0uB+9xuzRmI11oSudIjmb6nXP/H3ETot\n0CGMoZKO5XNE3u2Rua7AmN6qA4E+4fzWJUU3mC+IGI++68egHiYHs4XNwdsS1bFb\nL2cJLevdy+u8u0QujKCeCMfUIXRlIYu6yq6eTklwobke4Q1BgzVwwMIm5YlGV62V\nUcDL28FjcOXPXqD1OCi7VD7FZSGrp1EkNKtzKS9/HttEWNCOnvnJyV9vGrkxFUsj\nS6COhmoQgJiHipWjBmJ4Sb4ogVPEmOfr82R9g0RYXW6w+OPoxC8V/kZYZY644gFD\nScuhzuTLxBCeDvnRlG6VFWsuUr36hEUsp8THnQYB0nnoS10F3qiTW+PA/lFxs+Kd\nknZurr3Qf1qs8oyTGfj2wRRCS54E7rdQofJijQDCU+Zs1Ld3JP7fVxKyDNj+GoHJ\nHudcMtQc41S0ZGgb+zaRX9CW+fOJQ6S5XOV54nDSPoFPQVKGD2J2N/YpcemTRCA3\nmS5pG/qcePpIgXLnHnLxUxF56zjsqz3zpHO1VxXu3oEv6n8Oel8DonMCxfMtkvfd\nR2Y+XtGFIPiselGIOMxL4PdNNkesQfwzQww8oXcITUr+71KQDUNAE2arNYOkWbjS\nXgE8waAQIsE+W13wPEVJ7YtkrZivjKIxT1xYgM5D1ejFk+VAo25bz7hZFsvzxqcw\ncuZNNUbnQqbznrH4c7JzFAKhST49uy1enkGMQm1K7kr5XrAWRMc31YYF6I7nEfw=\n=c7uE\n-----END PGP MESSAGE-----",
+				"fp": "D6380604E0A09E002380524F0AECB9DCA67CB14C"
+			}
+		],
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.8.1"
+	}
+}
