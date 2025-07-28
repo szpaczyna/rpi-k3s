ed11f5ad3976bd1628e0ba2e11c454877e3cf62e
Michal Laszuk <shpaq@shpaq.org>
2025-07-28 09:19:33 +0200

    feat: updated deploy scripts with helm check and moved unused stuff

--- cluster/helm/influxdb/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#helm repo add influxdata https://helm.influxdata.com/
-#helm repo update
-sops -d ../../secrets/influxdb-auth.yaml > secret.yaml
-kubectl apply -f ./secret.yaml
-
-helm upgrade --install influxdb influxdata/influxdb -n apps -f ./values.yaml
-rm -rf secret.yaml
--- cluster/helm/loki/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#helm repo add grafana https://grafana.github.io/helm-charts
-#helm repo update
-
-helm upgrade --install loki --create-namespace --namespace logging grafana/loki-stack -f values.yaml
